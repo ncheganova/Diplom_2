2f6e38f45e2df31ddbd533b070fbfdfe7e352f40
Natalia Cheganova <ncheganova@yandex.ru>
2025-11-25 21:37:19 +0700

    email toLowerCase()

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -3,12 +3,13 @@ package data;
 import com.github.javafaker.Faker;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class TestData {
     public static final String BASE_URL = "https://stellarburgers.education-services.ru";
 
     static Faker userModel = new Faker();
-    public static final String EMAIL = userModel.name().lastName() + userModel.regexify("[0-9]{4}") + "@yandex.ru";
+    public static final String EMAIL = userModel.name().lastName().toLowerCase(Locale.ROOT) + userModel.regexify("[0-9]{4}") + "@yandex.ru";
     public static final String PASSWORD = userModel.regexify("[0-9]{4}");
     public static final String NAME = userModel.name().firstName();
     public static final String WRONG_EMAIL = "Wrong" + EMAIL;
